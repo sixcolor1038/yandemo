892bc39792142731cd106ac695aa7f2987c045c5
sixcolor <sixcolor1038@163.com>
2024-07-15 15:36:52 +0800

    简单demo

--- monolith-demo/src/main/java/com/yan/demo/easydemo/ChineseCharacterCountInFolder.java
+++ monolith-demo/src/main/java/com/yan/demo/easydemo/ChineseCharacterCountInFolder.java
@@ -1,25 +1,36 @@
 package com.yan.demo.easydemo;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @Author: sixcolor
  * @Date: 2024-06-19 0:00
  * @Description: 统计一个文件夹下文件中汉字的数量
  */
-
 public class ChineseCharacterCountInFolder {
 
     public static void main(String[] args) {
-        String folderPath = "E:\\";
-        File folder = new File(folderPath);
-
         try {
+            String sqlQuery = "select * from COMMON_REC where ID = 11";
+            List<Map<String, Object>> maps = jdbcTemplate.queryForList(sqlQuery);
+            Object remark = maps.get(0).get("REMARK");
+            String folderPath = remark.toString();
+            System.out.println(folderPath);
+            File folder = new File(folderPath);
             int totalChineseCharCount = traverseAndCount(folder);
             System.out.println("------当前文件夹下总汉字数 " + folderPath + ": " + totalChineseCharCount);
+            String sqlUpdate = "UPDATE COMMON_REC SET VALUE = ? where ID = 11";
+            int update = jdbcTemplate.update(sqlUpdate, totalChineseCharCount);
+            System.out.println(update);
         } catch (IOException e) {
             System.err.println("Error reading files: " + e.getMessage());
             e.printStackTrace();
@@ -54,6 +65,23 @@ public class ChineseCharacterCountInFolder {
             }
             return chineseCharCount;
         }
+    }
+
+    private static final JdbcTemplate jdbcTemplate;
+
+    static {
+        String driver = "com.mysql.cj.jdbc.Driver";//mysql驱动
+        String url = "jdbc:mysql://192.168.100.12:13306/yan_demo";//连接地址
+        String user = "root";//用户
+        String password = "rootroot";//密码
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driver);
+        dataSource.setUsername(user);
+        dataSource.setPassword(password);
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
