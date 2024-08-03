2c277ba7805c1e52b8d9f603c1795e629b5463b4
sixcolor <sixcolor1038@163.com>
2024-08-03 22:53:11 +0800

    简单demo

--- monolith-demo/src/main/java/com/yan/demo/designpattern/easydemo/MallardDuck.java
+++ monolith-demo/src/main/java/com/yan/demo/designpattern/easydemo/MallardDuck.java
@@ -1,13 +1,21 @@
 package com.yan.demo.designpattern.easydemo;
 
+import com.yan.demo.designpattern.easydemo.service.impl.FlyWithWings;
+import com.yan.demo.designpattern.easydemo.service.impl.Quack;
+
 /**
  * @Author: sixcolor
  * @Date: 2024-08-03
  * @Description:
  */
 public class MallardDuck extends Duck {
-    @Override
-    public void display() {
-        System.out.println("外观是绿头");
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
     }
+
+    public void display() {
+        System.out.println("绿头鸭");
+    }
+
 }
