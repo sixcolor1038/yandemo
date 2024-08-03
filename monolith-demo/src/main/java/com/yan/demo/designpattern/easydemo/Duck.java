b35b268f252b205dbef3480f83210501453e76c1
sixcolor <sixcolor1038@163.com>
2024-08-03 22:53:03 +0800

    简单demo

--- monolith-demo/src/main/java/com/yan/demo/designpattern/easydemo/Duck.java
+++ monolith-demo/src/main/java/com/yan/demo/designpattern/easydemo/Duck.java
@@ -1,20 +1,33 @@
 package com.yan.demo.designpattern.easydemo;
 
+import com.yan.demo.designpattern.easydemo.service.FlyBehavior;
+import com.yan.demo.designpattern.easydemo.service.QuackBehavior;
+
 /**
  * @Author: sixcolor
  * @Date: 2024-08-03
  * @Description: 鸭子父类
  */
-public class Duck {
-    public void quack(){
-        System.out.println("呱呱叫");
-    }
+public abstract class Duck {
 
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public void performFly(){
+        flyBehavior.fly();
+    }
+    public void performQuack(){
+        quackBehavior.quack();
+    }
     public void swim(){
-        System.out.println("在游泳");
+        System.out.println("所有鸭子都会游泳");
     }
 
-    public void display(){
-        System.out.println("外观");
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
     }
 }
---
+++ monolith-demo/src/main/java/com/yan/demo/designpattern/easydemo/service/FlyBehavior.java
@@ -0,0 +1,11 @@
+package com.yan.demo.designpattern.easydemo.service;
+
+/**
+ * @Author: sixcolor
+ * @Date: 2024-08-03
+ * @Description:
+ */
+public interface FlyBehavior {
+
+    void fly();
+}
