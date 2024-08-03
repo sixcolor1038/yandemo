6388c47e6a2673566d27dfba3f24e5ec35ba3410
sixcolor <sixcolor1038@163.com>
2024-08-03 22:53:19 +0800

    简单demo

---
+++ monolith-demo/src/main/java/com/yan/demo/designpattern/easydemo/service/QuackBehavior.java
@@ -0,0 +1,10 @@
+package com.yan.demo.designpattern.easydemo.service;
+
+/**
+ * @Author: sixcolor
+ * @Date: 2024-08-03
+ * @Description:
+ */
+public interface QuackBehavior {
+    void quack();
+}
